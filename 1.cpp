135cf17133b2e575f62803f25ea614b6448a8191
Deborah Hatton <deborah.hatton811@gmail.com>
2013-02-27 11:59:49 -0500

    Update 1.cpp
    Friend Corrected what I thought the question was asking.

--- 1.cpp
+++ 1.cpp
@@ -9,18 +9,13 @@ int main ()
 string password;
 cout << "Enter your password: " << "\n";
 getline( cin, password, '\n' );
-if ( password == "45" )
-{
-cout << "Access allowed" << "\n";
-}
-else if ( password == "0098" )
+if ( password == "45" || password == "0098")
 {
 cout << "Access allowed" << "\n";
 }
 else
 {
 cout << "Bad password. Denied access!" << "\n";
-// returning is a convenient way to stop the program
 return 0;
 }
 }
