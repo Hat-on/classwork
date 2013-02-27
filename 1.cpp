e5be41a8ade386072e179f5f3333c4a7bcc26d06
Hat-on <deborah.hatton811@gmail.com>
2013-02-26 22:58:33 -0500

    C++ excercise 1

---
+++ 1.cpp
@@ -0,0 +1,26 @@
+/*PROBLEM: Implement a simple "password" system that takes a password in the form of a number. Make it
+so that either of two numbers is valid, but use only one if statement to do the check.*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+string password;
+cout << "Enter your password: " << "\n";
+getline( cin, password, '\n' );
+if ( password == "45" )
+{
+cout << "Access allowed" << "\n";
+}
+else if ( password == "0098" )
+{
+cout << "Access allowed" << "\n";
+}
+else
+{
+cout << "Bad password. Denied access!" << "\n";
+// returning is a convenient way to stop the program
+return 0;
+}
+}
