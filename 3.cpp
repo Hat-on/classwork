62a0c2fe145107fcc773ed449987720f5bd917cd
Hat-on <deborah.hatton811@gmail.com>
2013-03-01 00:39:35 -0500

    C++ excercise 3

---
+++ 3.cpp
@@ -0,0 +1,30 @@
+/* Write a small calculator that takes as input one of the four arithmetic operations, the two
+arguments to those operations, and then prints out the result.*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int first_argument;
+int second_argument;
+operator sign;
+cout << "Enter first argument: ";
+cin >> first_argument;
+cout << "Enter operation:";
+cin >> sign;
+cout << "Enter second argument: ";
+cin >> second_argument;
+
+if (sign == *) // I really don't understand why my IDE wont get through this line.
+{
+    cout << first_argument << " * " << second_argument << " = " <<
+first_argument * second_argument << endl;
+}
+
+/*cout << first_argument << " + " << second_argument << " = " <<
+first_argument + second_argument << endl;
+cout << first_argument << " / " << second_argument << " = " <<
+first_argument / second_argument << endl;
+cout << first_argument << " - " << second_argument << " = " <<
+first_argument - second_argument << endl; */
+}
